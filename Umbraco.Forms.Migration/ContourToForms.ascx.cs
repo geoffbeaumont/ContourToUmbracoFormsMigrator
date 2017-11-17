758c6c8452bf24d3c048a30c4582f603a968c65c
geoffbeaumont <geoff.beaumont@integrious.co.uk>
2017-11-17 12:31:38 +0000

    Put throw/catch back round button click handler

--- Umbraco.Forms.Migration/ContourToForms.ascx.cs
+++ Umbraco.Forms.Migration/ContourToForms.ascx.cs
@@ -17,19 +17,19 @@ namespace Umbraco.Forms.Migration
         protected void Button1_Click(object sender, EventArgs e)
         {
             var success = true;
-            //try
-            //{
+            try
+            {
                 var s = new MigrationService
                 {
                     IgnoreRecords = this.ignoreRecords.Checked
                 };
                 s.Migrate(TextBox1.Text);
-            //}
-            //catch (Exception ex)
-            //{
-            //    success = false;
-            //    Literal1.Text = "<h4>Oops something whent wrong</h4><h5>" + ex.Message + "</h5><p>" + ex.StackTrace +"</p>";
-            //}
+            }
+            catch (Exception ex)
+            {
+                success = false;
+                Literal1.Text = "<h4>Oops something whent wrong</h4><h5>" + ex.Message + "</h5><p>" + ex.StackTrace +"</p>";
+            }
             if(success)
                 Literal1.Text = "<h4>Migration completed<h4><p>Your forms have been migrated from the Contour instance.</p>";
             
